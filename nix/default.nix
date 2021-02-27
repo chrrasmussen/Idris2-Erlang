a20e28c9d9918b3c510c685fcab3b5a64196b368
Christian Rasmussen <christian.rasmussen@me.com>
2021-02-27 17:42:07 +0100

    Override build flags for Idris 2 in Nix build

--- nix/default.nix
+++ nix/default.nix
@@ -8,6 +8,7 @@ import sources.nixpkgs {
       idris2 = super.idris2.overrideAttrs
         (_: {
           src = sources.idris2;
+          buildFlags = [ "bootstrap" "SCHEME=scheme" ];
         });
       inherit (import sources.niv { pkgs = super; }) niv;
     })
